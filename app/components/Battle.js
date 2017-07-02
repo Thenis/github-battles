58d49519c79738cbdbbb6c56a76811ce3e057a36
thenis <d.stoilov96@gmail.com>
2017-07-02 16:02:16 +0300

    Added input fields for github battles. Added PlayeInput component to handle the inputs

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,108 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class Battle extends React.Component {
+class PlayerInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        let value = event.target.value;
+
+        this.setState(() => {
+            return {
+                username: value
+            }
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.props.onSubmit(this.props.id, this.state.username);
+    }
+
     render() {
         return (
+            <form className='column' onSubmit={this.handleSubmit}>
+                <label className='header' htmlFor='username'>{this.props.label}</label>
+                <input
+                    type='text'
+                    id='username'
+                    placeholder='github username'
+                    autoComplete='off'
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
+                <button
+                    className='button'
+                    type='submit'
+                    disabled={!this.state.username}>
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+PlayerInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired
+}
+
+export default class Battle extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playerOneName: '',
+            playerTwoName: '',
+            playerOneImage: null,
+            playerTwoImage: null
+        }
+        this.handeSubmit = this.handeSubmit.bind(this);
+    }
+
+    handeSubmit(id, username) {
+        this.setState(() => {
+            let newState = {};
+
+            newState[id + 'Name'] = username;
+            newState[id + 'Image'] = `https://github.com/${username}.png?size=200`;
+
+            return newState;
+        });
+    }
+
+    render() {
+        let playerOneName = this.state.playerOneName;
+        let playerTwoName = this.state.playerTwoName;
+
+        return (
             <div>
-                Battle!
+                <div className='row'>
+                    {!playerOneName && 
+                    <PlayerInput
+                        id='playerOne'
+                        label='Player One'
+                        onSubmit={this.handeSubmit}
+                    />}
+                    
+                    {!playerTwoName && 
+                    <PlayerInput
+                        id='playerTwo'
+                        label='Player Two'
+                        onSubmit={this.handeSubmit}
+                    />}
+                </div>
             </div>
         );
     }
